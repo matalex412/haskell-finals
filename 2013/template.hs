bc9ca9554970bca127617717ff94ca0b42686491
Matthew Alex <ma4723@ic.ac.uk>
2023-12-19 10:50:02 +0000

    Completed 2013 paper

--- 2013/template.hs
+++ 2013/template.hs
@@ -1,3 +1,6 @@
+import Data.List (minimumBy)
+import Data.Ord (comparing)
+
 type BinHeap a = [BinTree a]
 
 data BinTree a = Node a Int (BinHeap a)
@@ -6,63 +9,136 @@ data BinTree a = Node a Int (BinHeap a)
 --------------------------------------------------------------
 -- PART I
 
-key :: BinTree a -> a
-key
-  = undefined
+value :: BinTree a -> a
+value (Node x _ _)
+  = x
 
 rank :: BinTree a -> Int
-rank
-  = undefined
+rank (Node _ r _)
+  = r
 
 children :: BinTree a -> [BinTree a]
-children
-  = undefined
+children (Node _ _ ts)
+  = ts
 
+-- combineTrees :: Ord a => BinTree a -> BinTree a -> BinTree a
+-- combineTrees t1 t2
+--   = Node (value rt) (rank rt + 1) (ct : children rt)
+--   where (rt, ct) = if (value t1 < value t2) then (t1, t2) else (t2, t1)
+
+-- pattern matching cleaner than extractor functions
 combineTrees :: Ord a => BinTree a -> BinTree a -> BinTree a
-combineTrees 
-  = undefined
+combineTrees t@(Node v r h) t'@(Node v' _ h')
+  | v < v'    = Node v (r + 1) (t' : h)
+  | otherwise = Node v' (r + 1) (t : h')
+
 
 --------------------------------------------------------------
 -- PART II
 
 extractMin :: Ord a => BinHeap a -> a
-extractMin 
-  = undefined
+extractMin
+  = minimum . map value
 
+-- pattern matching better than head and tail
+-- t, t' preferred to t1, t2
 mergeHeaps :: Ord a => BinHeap a -> BinHeap a -> BinHeap a
-mergeHeaps 
-  = undefined
+mergeHeaps lh []
+  = lh
+mergeHeaps [] rh
+  = rh
+mergeHeaps h@(t : ts) h'@(t' : ts')
+  | rank t < rank t'   
+    = t : mergeHeaps ts h'
+  | rank t' < rank t    
+    = t' : mergeHeaps ts' h
+  | otherwise            
+    = mergeHeaps [combineTrees t t'] (mergeHeaps ts ts')
 
+
+-- check for point free
 insert :: Ord a => a -> BinHeap a -> BinHeap a
-insert 
-  = undefined
+insert x
+  = mergeHeaps [Node x 0 []]
 
+-- comments for helper functions
+-- deleteMin :: Ord a => BinHeap a -> BinHeap a
+-- deleteMin []
+--   = []
+-- deleteMin h
+--   = mergeHeaps (reverse (children rt)) remH
+--   where (rt, remH) = removeMin h
+
+-- remove :: Eq a => a -> BinHeap a -> BinHeap a
+-- remove _ [] = [] 
+-- remove v (t:ts)
+--  | value t == v  = ts
+--  | otherwise     = t : remove v ts
+
+-- removeMin :: Ord a => BinHeap a -> (BinTree a, BinHeap a)
+-- removeMin h
+--   = (rt, remove (value rt) h)
+--   where rt = minimumBy (comparing value) h
+
+-- remember edge case [] when using minimum
 deleteMin :: Ord a => BinHeap a -> BinHeap a
-deleteMin 
-  = undefined
+deleteMin []
+  = []
+deleteMin h
+  = mergeHeaps (reverse (children rt)) [t | t <- h, t /= rt]
+  where rt = minimumBy (comparing value) h
 
-remove :: Eq a => a -> BinHeap a -> BinHeap a
-remove
-  = undefined
-
-removeMin :: Ord a => BinHeap a -> (BinTree a, BinHeap a)
-removeMin
-  = undefined
-
+-- if reverse needed try switch from tail recursion
 binSort :: Ord a => [a] -> [a]
-binSort 
-  = undefined
+binSort xs
+  = sort (foldr insert [] xs)
+  where sort :: Ord a => BinHeap a -> [a]
+        sort []
+          = []
+        sort h 
+          = extractMin h : sort (deleteMin h)
 
 --------------------------------------------------------------
 -- PART III
 
+-- toBinary :: BinHeap a -> [Int]
+-- toBinary ts
+--   = numNodes [0..maxR] []
+--   where rs = map rank ts
+--         maxR  = maximum rs
+
+--         numNodes :: [Int] -> [Int] -> [Int]
+--         numNodes [] xs 
+--           = xs
+--         numNodes (i : is) xs
+--          | i `elem` rs = numNodes is (1 : xs)
+--          | otherwise   = numNodes is (0 : xs)
+
+-- making use of ordering of binheap
 toBinary :: BinHeap a -> [Int]
-toBinary
-  = undefined
+toBinary ts
+  = numNodes ts []
+  where numNodes :: BinHeap a -> [Int] -> [Int]
+        numNodes [] xs
+          = xs
+        numNodes h@(t : ts) xs
+          | rank t == length xs  = numNodes ts (1 : xs)
+          | otherwise            = numNodes h (0 : xs)
 
 binarySum :: [Int] -> [Int] -> [Int]
-binarySum
-  = undefined
+binarySum xs ys
+  = adder (reverse xs') (reverse ys') 0 []
+  where l = max (length xs) (length ys)
+        xs' = until (\zs -> length zs == l) (0 : ) xs
+        ys' = until (\zs -> length zs == l) (0 : ) ys
+
+        adder :: [Int] -> [Int] -> Int -> [Int] -> [Int]
+        adder (x : xs) (y : ys) c res
+          | sum <= 1  = adder xs ys 0 (sum : res)
+          | otherwise = adder xs ys 1 (sum `mod` 2 : res)
+          where sum = x + y + c
+        adder _ _ 0 res = res
+        adder _ _ 1 res = 1 : res
 
 ------------------------------------------------------
 -- Some sample trees...
